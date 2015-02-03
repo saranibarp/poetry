347ca8e61e95fb6cbdd0de4aaf04d11a1e600b56
Maria Aranibar <saranibarp@outlook.com>
2015-02-03 20:35:01 +0000

    I don't know if this message is important or not

---
+++ db/migrate/20150203202723_create_poems.rb
@@ -0,0 +1,12 @@
+class CreatePoems < ActiveRecord::Migration
+  def change
+    create_table :poems do |t|
+      t.string :title
+      t.string :author
+      t.text :body
+      t.string :source
+      t.boolean :published, default: false
+      t.timestamps null: false
+    end
+  end
+end
